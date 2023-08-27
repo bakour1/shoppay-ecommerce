06f044a1fbee6ebc1bafbd740ef56ec12cb8746a
bakour1 <bakour512@gmail.com>
2023-08-27 12:11:44 +0100

    19. Next auth setup

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,35 @@
+import NextAuth from 'next-auth';
+import TwitterProvider from 'next-auth/providers/twitter';
+import FacebookProvider from 'next-auth/providers/facebook';
+import GoogleProvider from 'next-auth/providers/google';
+import GitHubProvider from 'next-auth/providers/github';
+import Auth0Provider from 'next-auth/providers/auth0';
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    TwitterProvider({
+      clientId: process.env.TWITTER_ID,
+      clientSecret: process.env.TWITTER_SECRET,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      issuer: process.env.AUTH0_ISSUER,
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,17 @@ import styles from '@/styles/Home.module.scss';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
 import axios from 'axios';
+import { useSession } from 'next-auth/react';
 
 // const inter = Inter({ subsets: ['latin'] });
 
 export default function Home({ country }) {
+  const { data: session } = useSession();
+
   return (
     <div>
       <Header country={country} />
+      {session ? 'ok' : 'no'}
       <Footer country={country} />
     </div>
   );
