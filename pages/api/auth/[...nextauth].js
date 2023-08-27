36eff50bba16bc6f516355c1926167a6b896964b
bakour1 <bakour512@gmail.com>
2023-08-27 13:29:05 +0100

    20. JSON web tokens & Mongodb adapter

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,10 +3,12 @@ import TwitterProvider from 'next-auth/providers/twitter';
 import FacebookProvider from 'next-auth/providers/facebook';
 import GoogleProvider from 'next-auth/providers/google';
 import GitHubProvider from 'next-auth/providers/github';
-import Auth0Provider from 'next-auth/providers/auth0';
+
+import { MongoDBAdapter } from '@auth/mongodb-adapter';
+import clientPromise from './lib/mongodb';
 
 export const authOptions = {
-  // Configure one or more authentication providers
+  adapter: MongoDBAdapter(clientPromise),
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
@@ -24,12 +26,14 @@ export const authOptions = {
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
-    Auth0Provider({
-      clientId: process.env.AUTH0_CLIENT_ID,
-      clientSecret: process.env.AUTH0_CLIENT_SECRET,
-      issuer: process.env.AUTH0_ISSUER,
-    }),
   ],
+  pages: {
+    signIn: '/signin',
+  },
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.JWT_SECRET,
 };
 
 export default NextAuth(authOptions);
