19ef04638f9bf2225b6bc05500d1d00f3b647aaf
bakour1 <bakour512@gmail.com>
2023-08-27 11:05:15 +0100

    15. Footer Socials & Newsletter

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,8 +1,8 @@
 // import Copyright from './Copyright';
 import Links from './Links';
-// import NewsLetter from './NewsLetter';
+import NewsLetter from './NewsLetter';
 // import Payment from './Payment';
-// import Socials from './Socials';
+import Socials from './Socials';
 import styles from './styles.module.scss';
 
 export default function Footer({ country }) {
@@ -10,9 +10,9 @@ export default function Footer({ country }) {
     <footer className={styles.footer}>
       <div className={styles.footer__container}>
         <Links />
-        {/* <Socials />
+        <Socials />
         <NewsLetter />
-        <Payment />
+        {/* <Payment />
         <Copyright country={country} /> */}
       </div>
     </footer>
