278a7995419958d783fb23fb5fd43ca4447f32d7
bakour1 <bakour512@gmail.com>
2023-08-29 00:06:04 +0100

    Auth0Provider({       clientId: process.env.AUTH0_CLIENT_ID,       clientSecret: process.env.AUTH0_CLIENT_SECRET,       issuer: process.env.AUTH0_ISSUER, // Use the correct environment variable     }),

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,4 +1,5 @@
 import NextAuth from 'next-auth';
+import Auth0Provider from 'next-auth/providers/auth0';
 import TwitterProvider from 'next-auth/providers/twitter';
 import FacebookProvider from 'next-auth/providers/facebook';
 import GoogleProvider from 'next-auth/providers/google';
@@ -25,6 +26,11 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      issuer: process.env.AUTH0_ISSUER, // Use the correct environment variable
     }),
   ],
   pages: {
