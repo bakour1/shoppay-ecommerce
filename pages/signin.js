496185ceac00206fad6b0fbf9479551c05b93e01
bakour1 <bakour512@gmail.com>
2023-08-29 00:52:15 +0100

    28. Sign in page 3 - Custom input with yup

--- pages/signin.js
+++ pages/signin.js
@@ -4,9 +4,39 @@ import styles from '../styles/signin.module.scss';
 import { BiLeftArrowAlt } from 'react-icons/bi';
 import Link from 'next/link';
 import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
 import LoginInput from '@/components/inputs/loginInput';
+import { useState } from 'react';
+
+const initialvalues = {
+  login_email: '',
+  login_password: '',
+  name: '',
+  email: '',
+  password: '',
+  conf_password: '',
+  success: '',
+  error: '',
+  login_error: '',
+};
 
 export default function signin() {
+  const [user, setUser] = useState(initialvalues);
+  const { login_email, login_password } = user;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+  console.log(user);
+
+  const loginValidation = Yup.object({
+    login_email: Yup.string()
+      .required('Email address is required.')
+      .email('Please enter a valid email address.'),
+    login_password: Yup.string().required('Please enter a password'),
+  });
+
   const country = {
     name: 'Morocco',
     flag: 'https://cdn-icons-png.flaticon.com/512/197/197551.png?w=360',
@@ -29,11 +59,30 @@ export default function signin() {
             <p>
               Get access to one of the best Eshopping services in the world.
             </p>
-            <Formik>
+            <Formik
+              enableReinitialize
+              initialValues={{
+                login_email,
+                login_password,
+              }}
+              validationSchema={loginValidation}
+            >
               {(form) => (
                 <Form method="post" action="/api/auth/signin/email">
-                  <LoginInput icon="email" placeholder="Email Address" />
-                  <LoginInput icon="password" placeholder="Password" />
+                  <LoginInput
+                    type="text"
+                    name="login_email"
+                    icon="email"
+                    placeholder="Email Address"
+                    onChange={handleChange}
+                  />
+                  <LoginInput
+                    type="password"
+                    name="login_password"
+                    icon="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                  />
                 </Form>
               )}
             </Formik>
