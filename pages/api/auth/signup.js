4b95525271803cfda95624f76c45959a163ca5b4
bakour1 <bakour512@gmail.com>
2023-08-29 01:59:55 +0100

    32. Sign up 1

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,21 @@
+import nc from 'next-connect';
+import { validateEmail } from '../../../utils/validation';
+import db from '../../../utils/db';
+const handler = nc();
+
+handler.post(async (req, res) => {
+  try {
+    await db.connectDb();
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: 'Please fill in all fields.' });
+    }
+    if (!validateEmail(email)) {
+      return res.status(400).json({ message: 'Invalid email.' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+export default handler;
