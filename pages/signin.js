8e9068bb6b99dcc44d7a43f829d2ac3003195c9d
bakour1 <bakour512@gmail.com>
2023-08-29 01:05:30 +0100

    29. Sign in page 4 button submit

--- pages/signin.js
+++ pages/signin.js
@@ -7,6 +7,7 @@ import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 import LoginInput from '@/components/inputs/loginInput';
 import { useState } from 'react';
+import CircledIconBtn from '@/components/buttons/circledIconBtn';
 
 const initialvalues = {
   login_email: '',
@@ -83,6 +84,10 @@ export default function signin() {
                     placeholder="Password"
                     onChange={handleChange}
                   />
+                  <CircledIconBtn type="submit" text="Sign in" />
+                  <div className={styles.forgot}>
+                    <Link href="/auth/forgot">Forgot password ?</Link>
+                  </div>
                 </Form>
               )}
             </Formik>
