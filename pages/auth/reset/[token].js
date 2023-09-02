a4c1bb291d97d401e763e9a35a05bf8726b8dc79
bakour1 <bakour512@gmail.com>
2023-09-02 06:27:20 +0100

    45.46 Reset password

--- pages/auth/reset/[token].js
+++ pages/auth/reset/[token].js
@@ -1,7 +1,132 @@
-import React from 'react';
+/* eslint-disable react-hooks/rules-of-hooks */
+import styles from '../../../styles/forgot.module.scss';
+import Header from '../../../components/header';
+import Footer from '../../../components/footer';
+import { BiLeftArrowAlt } from 'react-icons/bi';
+import CircledIconBtn from '../../../components/buttons/circledIconBtn';
+import LoginInput from '../../../components/inputs/loginInput';
+import { useState } from 'react';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+import Link from 'next/link';
+import DotLoaderSpinner from '@/components/dotLoader';
+import axios from 'axios';
+import { getSession, signIn } from 'next-auth/react';
+import jwt from 'jsonwebtoken';
+import { Router } from 'next/router';
+export default function reset({ user_id }) {
+  console.log('user_id', user_id);
+  const [password, setPassword] = useState('');
+  const [conf_password, setConf_password] = useState('');
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+  const [loading, setLoading] = useState('');
+  const passwordValidation = Yup.object({
+    password: Yup.string()
+      .required('Please enter your new password.')
+      .min(6, 'Password must be atleast 6 characters.')
+      .max(36, "Password can't be more than 36 characters"),
+    conf_password: Yup.string()
+      .required('Confirm your password.')
+      .oneOf([Yup.ref('password')], 'Passwords must match.'),
+  });
+  const resetHandler = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.put('/api/auth/reset', {
+        user_id,
+        password,
+      });
+      let options = {
+        redirect: false,
+        email: data.email,
+        password: password,
+      };
+      await signIn('credentials', options);
+      window.location.reload(true);
+    } catch (error) {
+      setLoading(false);
+      setSuccess('');
+      setError(error.response.data.message);
+    }
+  };
+  return (
+    <>
+      {loading && <DotLoaderSpinner loading={loading} />}
+      <Header country="" />
+      <div className={styles.forgot}>
+        <div>
+          <div className={styles.forgot__header}>
+            <div className={styles.back__svg}>
+              <BiLeftArrowAlt />
+            </div>
+            <span>
+              Reset your password ? <Link href="/">Login instead</Link>
+            </span>
+          </div>
+          <Formik
+            enableReinitialize
+            initialValues={{
+              password,
+              conf_password,
+            }}
+            validationSchema={passwordValidation}
+            onSubmit={() => {
+              resetHandler();
+            }}
+          >
+            {(form) => (
+              <Form>
+                <LoginInput
+                  type="password"
+                  name="password"
+                  icon="password"
+                  placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <LoginInput
+                  type="password"
+                  name="conf_password"
+                  icon="password"
+                  placeholder="Confirm Password"
+                  onChange={(e) => setConf_password(e.target.value)}
+                />
 
-const ResetPage = () => {
-  return <div>ResetPage</div>;
-};
+                <CircledIconBtn type="submit" text="Submit" />
+                <div style={{ marginTop: '10px' }}>
+                  {error && <span className={styles.error}>{error}</span>}
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      </div>
+      <Footer country="" />
+    </>
+  );
+}
 
-export default ResetPage;
+export async function getServerSideProps(context) {
+  const { query, req } = context;
+  const session = await getSession({ req });
+  console.log('============test');
+  if (session) {
+    console.log('===========ok');
+    return {
+      redirect: {
+        destination: '/',
+      },
+    };
+  }
+  const token = query.token;
+  const user_id = jwt.verify(token, process.env.RESET_TOKEN_SECRET);
+  if (user_id == null) {
+    console.log('xxxxxxxxxxxxx');
+  }
+  console.log(user_id); // { id: '64f2bf3ce7dfcc32bfbb5dbc', iat: 1693630295, exp: 1693651895 }
+  return {
+    props: {
+      user_id: user_id.id,
+    },
+  };
+}
