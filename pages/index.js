61e62f987f7881108a580d515dfe67d734509236
bakour1 <bakour512@gmail.com>
2023-09-03 13:42:38 +0100

    60. Category cards with Respnsive

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,16 @@ import axios from 'axios';
 import { useSession } from 'next-auth/react';
 import Main from '@/components/home/main';
 import FlashDeals from '@/components/home/flashDeals';
+import Category from '@/components/home/category';
+import { women_accessories, women_dresses, women_shoes } from '@/data/home';
 // const inter = Inter({ subsets: ['latin'] });
+import { useMediaQuery } from 'react-responsive';
 
 export default function Home({ country }) {
   const { data: session } = useSession();
   console.log(session);
+  const isMedium = useMediaQuery({ query: '(max-width:850px)' });
+  const isMobile = useMediaQuery({ query: '(max-width:550px)' });
 
   return (
     <div>
@@ -20,6 +25,32 @@ export default function Home({ country }) {
         <div className={styles.container}>
           <Main />
           <FlashDeals />
+          <div className={styles.home__category}>
+            <Category
+              header="Dresses"
+              products={women_dresses}
+              background="#5a31f4"
+            />
+            {!isMedium && (
+              <Category
+                header="Shoes"
+                products={women_shoes}
+                background="#3c811f"
+              />
+            )}
+            {isMobile && (
+              <Category
+                header="Shoes"
+                products={women_shoes}
+                background="#3c811f"
+              />
+            )}
+            <Category
+              header="Accessories"
+              products={women_accessories}
+              background="#000"
+            />
+          </div>
         </div>
       </div>
       <Footer country={country} />
