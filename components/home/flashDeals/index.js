a9cd02d82c9951d43859b985fadc3a300b12d908
bakour1 <bakour512@gmail.com>
2023-09-03 22:45:46 +0100

    70. Get, filter and prepare product data

--- components/home/flashDeals/index.js
+++ components/home/flashDeals/index.js
@@ -21,7 +21,7 @@ export default function FlashDeals() {
           FLASH SALE
           <MdFlashOn />
         </h1>
-        <Countdown date={new Date(2023, 8, 23, 0)} />
+        <Countdown date={new Date(2024, 12, 30)} />
       </div>
       <Swiper
         slidesPerView={1}
