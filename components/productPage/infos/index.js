6f5f8a83bb994ca3f169d07d6822e6424c8a2044
bakour1 <bakour512@gmail.com>
2023-09-05 19:56:20 +0100

    77. product infos 5 accordian

--- components/productPage/infos/index.js
+++ components/productPage/infos/index.js
@@ -8,6 +8,7 @@ import Link from 'next/link';
 import { TbPlus, TbMinus } from 'react-icons/tb';
 import { BsHandbagFill, BsHeart } from 'react-icons/bs';
 import Share from '../share';
+import Accordian from './Accordian';
 export default function Infos({ product, setActiveImg }) {
   console.log(product);
   const router = useRouter();
@@ -125,6 +126,7 @@ export default function Infos({ product, setActiveImg }) {
           </button>
         </div>
         <Share />
+        <Accordian details={[product.description, ...product.details]} />
       </div>
     </div>
   );
