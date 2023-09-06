5ec80231d5ded4fcc19bffb2d2e97e78904f7bf3
bakour1 <bakour512@gmail.com>
2023-09-06 21:32:42 +0100

    86. reviews table header

--- components/productPage/reviews/Table.js
+++ components/productPage/reviews/Table.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import usePagination from './Pagination';
 import Review from './Review';
 import styles from './styles.module.scss';
+import TableHeader from './TableHeader';
 
 export default function Table({ reviews, allSizes, colors }) {
   const [page, setPage] = useState(1);
@@ -16,6 +17,11 @@ export default function Table({ reviews, allSizes, colors }) {
 
   return (
     <div className={styles.table}>
+      <TableHeader
+        reviews={reviews}
+        allSizes={[{ size: 'All' }, ...allSizes]}
+        colors={[{ color: '', image: '' }, ...colors]}
+      />
       <div className={styles.table__data}>
         {_DATA.currentData().map((review, i) => (
           <Review review={review} key={i} />
