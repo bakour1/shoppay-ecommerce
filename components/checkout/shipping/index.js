ab2e912bf5dd66153bec95e37e73b80bc22b4abd
bakour1 <bakour512@gmail.com>
2023-09-10 18:03:05 +0100

    101. Checkout ,get cart, work on shipping address form 1
    & 102. custom shipping input & 103. shipping form full markup and styles

---
+++ components/checkout/shipping/index.js
@@ -0,0 +1,166 @@
+import styles from './styles.module.scss';
+import { useState } from 'react';
+import { Form, Formik } from 'formik';
+import * as Yup from 'yup';
+import 'yup-phone';
+import ShippingInput from '../../inputs/shippingInput';
+import { countries } from '../../../data/countries';
+import SingularSelect from '../../selects/SingularSelect';
+
+const initialValues = {
+  firstName: '',
+  lastName: '',
+  phoneNumber: '',
+  state: '',
+  city: '',
+  zipCode: '',
+  address1: '',
+  address2: '',
+  country: '',
+};
+
+export default function Shipping({ user, addresses, setAddresses, profile }) {
+  const [shipping, setShipping] = useState(initialValues);
+  const [visible, setVisible] = useState(user?.address.length ? false : true);
+  const {
+    firstName,
+    lastName,
+    phoneNumber,
+    state,
+    city,
+    zipCode,
+    address1,
+    address2,
+    country,
+  } = shipping;
+
+  const validate = Yup.object({
+    firstName: Yup.string()
+      .required('First name is required.')
+      .min(3, 'First name must be atleast 3 characters long.')
+      .max(20, 'First name must be less than 20 characters long.'),
+    lastName: Yup.string()
+      .required('Last name is required.')
+      .min(3, 'Last name must be atleast 3 characters long.')
+      .max(20, 'Last name must be less than 20 characters long.'),
+    phoneNumber: Yup.string()
+      .required('Phone number is required.')
+      .phone()
+      .min(3, 'Phone number must be atleast 3 characters long.')
+      .max(30, 'Phone number must be less than 20 characters long.'),
+    state: Yup.string()
+      .required('State name is required.')
+      .min(2, 'State name should contain 2-60 characters..')
+      .max(60, 'State name should contain 2-60 characters.'),
+    city: Yup.string()
+      .required('City name is required.')
+      .min(2, 'City name should contain 2-60 characters.')
+      .max(60, 'City name should contain 2-60 characters.'),
+    zipCode: Yup.string()
+      .required('ZipCode/Postal is required.')
+      .min(2, 'ZipCode/Postal should contain 2-30 characters..')
+      .max(30, 'ZipCode/Postal should contain 2-30 characters.'),
+    address1: Yup.string()
+      .required('Address Line 1 is required.')
+      .min(5, 'Address Line 1 should contain 5-100 characters.')
+      .max(100, 'Address Line 1 should contain 5-100 characters.'),
+    address2: Yup.string()
+      .min(5, 'Address Line 2 should contain 5-100 characters.')
+      .max(100, 'Address Line 2 should contain 5-100 characters.'),
+    country: Yup.string().required('Country name is required.'),
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setShipping({ ...shipping, [name]: value });
+  };
+  const saveShippingHandler = async () => {};
+
+  return (
+    <div className={styles.shipping}>
+      {!profile && (
+        <div className={styles.header}>
+          <h3>Shipping Informations</h3>
+        </div>
+      )}
+      {visible && (
+        <Formik
+          enableReinitialize
+          initialValues={{
+            firstName,
+            lastName,
+            phoneNumber,
+            state,
+            city,
+            zipCode,
+            address1,
+            address2,
+            country,
+          }}
+          validationSchema={validate}
+          onSubmit={() => {
+            saveShippingHandler();
+          }}
+        >
+          {(formik) => (
+            <Form>
+              <SingularSelect
+                name="country"
+                value={country}
+                placeholder="*Country"
+                handleChange={handleChange}
+                data={countries}
+              />
+              <div className={styles.col}>
+                <ShippingInput
+                  name="firstName"
+                  placeholder="*First Name"
+                  onChange={handleChange}
+                />
+                <ShippingInput
+                  name="lastName"
+                  placeholder="*Last Name"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className={styles.col}>
+                <ShippingInput
+                  name="state"
+                  placeholder="*State/Province"
+                  onChange={handleChange}
+                />
+                <ShippingInput
+                  name="city"
+                  placeholder="*City"
+                  onChange={handleChange}
+                />
+              </div>
+              <ShippingInput
+                name="phoneNumber"
+                placeholder="*Phone number"
+                onChange={handleChange}
+              />
+              <ShippingInput
+                name="zipCode"
+                placeholder="*Post/Zip code"
+                onChange={handleChange}
+              />
+              <ShippingInput
+                name="address1"
+                placeholder="Address 1"
+                onChange={handleChange}
+              />
+              <ShippingInput
+                name="address2"
+                placeholder="Address 2"
+                onChange={handleChange}
+              />
+              <button type="submit">Save Address</button>
+            </Form>
+          )}
+        </Formik>
+      )}
+    </div>
+  );
+}
+//
---
+++ pages/checkout.js
@@ -0,0 +1,56 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useState, useEffect } from 'react';
+import styles from '../styles/checkout.module.scss';
+import { getSession } from 'next-auth/react';
+import db from '../utils/db';
+import User from '@/models/User';
+import Cart from '@/models/Cart';
+import Header from '../components/cart/header';
+import Shipping from '../components/checkout/shipping';
+
+export default function checkout({ cart, user }) {
+  const [addresses, setAddresses] = useState(user?.address || []);
+
+  return (
+    <>
+      <Header />
+      <div className={`${styles.container} ${styles.checkout}`}>
+        <div className={styles.checkout__side}>
+          <Shipping
+            user={user}
+            addresses={addresses}
+            setAddresses={setAddresses}
+          />
+        </div>
+        <div className={styles.checkout__side}></div>
+      </div>
+    </>
+  );
+}
+export async function getServerSideProps(context) {
+  db.connectDb();
+  const session = await getSession(context);
+  const user = await User.findById(session.user.id);
+  if (!user) {
+    return {
+      redirect: {
+        destination: '/signin',
+      },
+    };
+  }
+  const cart = await Cart.findOne({ user: user._id });
+  db.disconnectDb();
+  if (!cart) {
+    return {
+      redirect: {
+        destination: '/cart',
+      },
+    };
+  }
+  return {
+    props: {
+      cart: JSON.parse(JSON.stringify(cart)),
+      user: JSON.parse(JSON.stringify(user)),
+    },
+  };
+}
