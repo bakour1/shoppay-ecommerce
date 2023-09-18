5b5ed776568b9ac1c41324b804a4cb90b12dc659
bakour1 <bakour512@gmail.com>
2023-09-18 20:34:11 +0100

    117. create order page and adding coupon to order

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -1,7 +1,186 @@
-import React from 'react';
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable react-hooks/rules-of-hooks */
+import styles from '../../styles/order.module.scss';
+import Header from '../../components/header';
+import Order from '../../models/Order';
+import User from '../../models/User';
+import { IoIosArrowForward } from 'react-icons/io';
+import db from '../../utils/db';
 
-function orderPage() {
-  return <div>orderPage</div>;
+export default function order({ orderData }) {
+  return (
+    <>
+      <Header country="country" />
+      <div className={styles.order}>
+        <div className={styles.container}>
+          <div className={styles.order__infos}>
+            <div className={styles.order__header}>
+              <div className={styles.order__header_head}>
+                Home <IoIosArrowForward /> Orders <IoIosArrowForward /> ID{' '}
+                {orderData._id}
+              </div>
+              <div className={styles.order__header_status}>
+                Payment Status :{' '}
+                {orderData.isPaid ? (
+                  <img src="../../../images/verified.png" alt="paid" />
+                ) : (
+                  <img src="../../../images/unverified.png" alt="paid" />
+                )}
+              </div>
+              <div className={styles.order__header_status}>
+                Order Status :
+                <span
+                  className={
+                    orderData.status == 'Not Processed'
+                      ? styles.not_processed
+                      : orderData.status == 'Processing'
+                      ? styles.processing
+                      : orderData.status == 'Dispatched'
+                      ? styles.dispatched
+                      : orderData.status == 'Cancelled'
+                      ? styles.cancelled
+                      : orderData.status == 'Completed'
+                      ? styles.completed
+                      : ''
+                  }
+                >
+                  {orderData.status}
+                </span>
+              </div>
+            </div>
+            <div className={styles.order__products}>
+              {orderData.products.map((product) => (
+                <div className={styles.product} key={product._id}>
+                  <div className={styles.product__img}>
+                    <img src={product.image} alt={product.name} />
+                  </div>
+                  <div className={styles.product__infos}>
+                    <h1 className={styles.product__infos_name}>
+                      {product.name.length > 30
+                        ? `${product.name.substring(0, 30)}...`
+                        : product.name}
+                    </h1>
+                    <div className={styles.product__infos_style}>
+                      <img src={product.color.image} alt="" /> / {product.size}
+                    </div>
+                    <div className={styles.product__infos_priceQty}>
+                      {product.price}$ x {product.qty}
+                    </div>
+                    <div className={styles.product__infos_total}>
+                      {product.price * product.qty}$
+                    </div>
+                  </div>
+                </div>
+              ))}
+              <div className={styles.order__products_total}>
+                {orderData.couponApplied ? (
+                  <>
+                    <div className={styles.order__products_total_sub}>
+                      <span>Subtotal</span>
+                      <span>{orderData.totalBeforeDiscount}$</span>
+                    </div>
+                    <div className={styles.order__products_total_sub}>
+                      <span>
+                        Coupon Applied <em>({orderData.couponApplied})</em>{' '}
+                      </span>
+                      <span>
+                        -
+                        {(
+                          orderData.totalBeforeDiscount - orderData.total
+                        ).toFixed(2)}
+                        $
+                      </span>
+                    </div>
+                    <div className={styles.order__products_total_sub}>
+                      <span>Tax price</span>
+                      <span>+{orderData.taxPrice}$</span>
+                    </div>
+                    <div
+                      className={`${styles.order__products_total_sub} ${styles.bordertop}`}
+                    >
+                      <span>TOTAL TO PAY</span>
+                      <b>{orderData.total}$</b>
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <div className={styles.order__products_total_sub}>
+                      <span>Tax price</span>
+                      <span>+{orderData.taxPrice}$</span>
+                    </div>
+                    <div
+                      className={`${styles.order__products_total_sub} ${styles.bordertop}`}
+                    >
+                      <span>TOTAL TO PAY</span>
+                      <b>{orderData.total}$</b>
+                    </div>
+                  </>
+                )}
+              </div>
+            </div>
+          </div>
+          <div className={styles.order__actions}>
+            <div className={styles.order__address}>
+              <h1>Customer s Order</h1>
+              <div className={styles.order__address_user}>
+                <div className={styles.order__address_user_infos}>
+                  <img src={orderData.user.image} alt="" />
+                  <div>
+                    <span>{orderData.user.name}</span>
+                    <span>{orderData.user.email}</span>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.order__address_shipping}>
+                <h2>Shipping Address</h2>
+                <span>
+                  {orderData.shippingAddress.firstName}{' '}
+                  {orderData.shippingAddress.lastName}
+                </span>
+                <span>{orderData.shippingAddress.address1}</span>
+                <span>{orderData.shippingAddress.address2}</span>
+                <span>
+                  {orderData.shippingAddress.state},
+                  {orderData.shippingAddress.city}
+                </span>
+                <span>{orderData.shippingAddress.zipCode}</span>
+                <span>{orderData.shippingAddress.country}</span>
+              </div>
+              <div className={styles.order__address_shipping}>
+                <h2>Billing Address</h2>
+                <span>
+                  {orderData.shippingAddress.firstName}{' '}
+                  {orderData.shippingAddress.lastName}
+                </span>
+                <span>{orderData.shippingAddress.address1}</span>
+                <span>{orderData.shippingAddress.address2}</span>
+                <span>
+                  {orderData.shippingAddress.state},
+                  {orderData.shippingAddress.city}
+                </span>
+                <span>{orderData.shippingAddress.zipCode}</span>
+                <span>{orderData.shippingAddress.country}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default orderPage;
+export async function getServerSideProps(context) {
+  db.connectDb();
+  const { query } = context;
+  const id = query.id;
+  const order = await Order.findById(id)
+    .populate({ path: 'user', model: User })
+    .lean();
+
+  db.disconnectDb();
+  return {
+    props: {
+      orderData: JSON.parse(JSON.stringify(order)),
+    },
+  };
+}
