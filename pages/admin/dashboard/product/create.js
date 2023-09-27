c514f8897332db3f400c8c28cdb23067980b366d
bakour1 <bakour512@gmail.com>
2023-09-27 20:32:56 +0100

    152. form  - Singular Select - Multiple-Select

--- pages/admin/dashboard/product/create.js
+++ pages/admin/dashboard/product/create.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable react-hooks/rules-of-hooks */
 import styles from '../../../../styles/products.module.scss';
 import Layout from '../../../../components/admin/layout';
@@ -5,7 +6,25 @@ import db from '../../../../utils/db';
 import Product from '../../../../models/Product';
 import Category from '../../../../models/Category';
 import { useEffect, useState } from 'react';
+// import { toast } from 'react-toastify';
 import axios from 'axios';
+import * as Yup from 'yup';
+import { Form, Formik } from 'formik';
+import SingularSelect from '../../../../components/selects/SingularSelect';
+import MultipleSelect from '../../../../components/selects/MultipleSelect';
+import AdminInput from '../../../../components/inputs/adminInput';
+// import DialogModal from '../../../../components/dialogModal';
+// import { useDispatch } from 'react-redux';
+// import { showDialog } from '../../../../store/DialogSlice';
+// import Images from '../../../../components/admin/createProduct/images';
+// import Colors from '../../../../components/admin/createProduct/colors';
+// import Style from '../../../../components/admin/createProduct/style';
+// import Sizes from '../../../../components/admin/createProduct/clickToAdd/Sizes';
+// import Details from '../../../../components/admin/createProduct/clickToAdd/Details';
+// import Questions from '../../../../components/admin/createProduct/clickToAdd/Questions';
+import { validateCreateProduct } from '../../../../utils/validation';
+// import dataURItoBlob from '../../../../utils/dataURItoBlob';
+// import { uploadImages } from '../../../../requests/upload';
 
 const initialState = {
   name: '',
@@ -47,11 +66,17 @@ const initialState = {
 export default function create({ parents, categories }) {
   const [product, setProduct] = useState(initialState);
   const [subs, setSubs] = useState([]);
+  const [colorImage, setColorImage] = useState('');
+  const [images, setImages] = useState([]);
+  const [description_images, setDescription_images] = useState('');
+  const [loading, setLoading] = useState(false);
+  //   const dispatch = useDispatch();
+  console.log('product', product);
 
   useEffect(() => {
     const getParentData = async () => {
       const { data } = await axios.get(`/api/product/${product.parent || ''}`);
-      console.log(data);
+      console.log('data', data);
       if (data) {
         setProduct({
           ...product,
@@ -76,14 +101,144 @@ export default function create({ parents, categories }) {
           category: product.category,
         },
       });
-      console.log(data);
+      console.log('data', data);
       setSubs(data);
     }
     getSubs();
   }, [product.category]);
+
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+    setProduct({ ...product, [name]: value });
+  };
+
+  const validate = Yup.object({
+    name: Yup.string()
+      .required('Please add a name')
+      .min(10, 'Product name must bewteen 10 and 300 characters.')
+      .max(300, 'Product name must bewteen 10 and 300 characters.'),
+    brand: Yup.string().required('Please add a brand'),
+    category: Yup.string().required('Please select a category.'),
+    /*
+      subCategories: Yup.array().min(
+        1,
+        "Please select atleast one sub Category."
+      ),
+     */
+    sku: Yup.string().required('Please add a sku/number'),
+    color: Yup.string().required('Please add a color'),
+    description: Yup.string().required('Please add a description'),
+  });
+
+  const createProduct = async () => {};
+
   return (
     <Layout>
       <div className={styles.header}>Create Product</div>
+      <Formik
+        enableReinitialize
+        initialValues={{
+          name: product.name,
+          brand: product.brand,
+          description: product.description,
+          category: product.category,
+          subCategories: product.subCategories,
+          parent: product.parent,
+          sku: product.sku,
+          discount: product.discount,
+          color: product.color.color,
+          imageInputFile: '',
+          styleInout: '',
+        }}
+        validationSchema={validate}
+        onSubmit={() => {
+          createProduct();
+        }}
+      >
+        {(formik) => (
+          <Form>
+            <div className={styles.flex}>
+              {product.color.image && (
+                <img
+                  src={product.color.image}
+                  className={styles.image_span}
+                  alt=""
+                />
+              )}
+              {product.color.color && (
+                <span
+                  className={styles.color_span}
+                  style={{ background: `${product.color.color}` }}
+                ></span>
+              )}
+            </div>
+
+            <SingularSelect
+              data={parents}
+              handleChange={handleChange}
+              placeholder="Parent product"
+              header="Add to an existing product"
+              name="parent"
+              value={product.parent}
+            />
+            <SingularSelect
+              name="category"
+              value={product.category}
+              placeholder="Category"
+              data={categories}
+              header="Select a Category"
+              handleChange={handleChange}
+              disabled={product.parent}
+            />
+            {product.category && (
+              <MultipleSelect
+                value={product.subCategories}
+                data={subs}
+                header="Select SubCategories"
+                name="subCategories"
+                disabled={product.parent}
+                handleChange={handleChange}
+              />
+            )}
+            <div className={styles.header}>Basic Infos</div>
+            <AdminInput
+              type="text"
+              label="Name"
+              name="name"
+              placeholder="Product name"
+              onChange={handleChange}
+            />
+            <AdminInput
+              type="text"
+              label="Description"
+              name="description"
+              placeholder="Product description"
+              onChange={handleChange}
+            />
+            <AdminInput
+              type="text"
+              label="Brand"
+              name="brand"
+              placeholder="Product brand"
+              onChange={handleChange}
+            />
+            <AdminInput
+              type="text"
+              label="Sku"
+              name="sku"
+              placeholder="Product sku/ number"
+              onChange={handleChange}
+            />
+            <AdminInput
+              type="text"
+              label="Discount"
+              name="discount"
+              placeholder="Product discount"
+              onChange={handleChange}
+            />
+          </Form>
+        )}
+      </Formik>
     </Layout>
   );
 }
